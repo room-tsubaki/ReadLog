25408a106dc9a09c97b9d832e4c4b74c44f529a9
Daiki Hayata <daikihayata0606@gmail.com>
2019-01-30 10:22:39 +0900

    topの修正、assets.rbでCSSプリコンパイルの追記、home.scssへの追記、application.cssにcss読み込み順番の追記

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( application_top.css )
+Rails.application.config.assets.precompile += %w( bootstrap_css/bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( vertical-timeline-css/reset.css )
+Rails.application.config.assets.precompile += %w( vertical-timeline-css/style.css )
